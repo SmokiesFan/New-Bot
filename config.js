b59f00e93b8939957344e6a341236b025e7324c0
Vanxh <vanxhyt@gmail.com>
2021-12-09 22:12:12 +0530

    First commit
    Enjoy free auto posting gameflip bot :)

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    discord: {
+        status: 'Gameflip AutoListing',
+        statusType: 'PLAYING',
+        token: 'add your discord bot token here lol',
+        prefix: '$',
+        ownerIDs: ['727224012912197652' , '741898574815821868'],
+        ownerOnly: false
+    }
+
+}
